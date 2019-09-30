2ba862934f9680257933664b50a57f02aa593180
GabrielLacerda <gabriellacerdadesign@gmail.com>
2019-09-30 20:37:17 -0300

    Added port config to database

--- config/database.js
+++ config/database.js
@@ -8,5 +8,6 @@ module.exports = {
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT
+  dialect: process.env.DB_DIALECT,
+  port: process.env.DB_PORT
 }
