e2ca6da642b7193b0063b56c64ac7ece4a804235
Deepak Bhattarai <bring2dip@gmail.com>
2019-12-19 10:53:57 +0545

    fix logger file illegal characters copied from slack

--- server/logger.js
+++ server/logger.js
@@ -1,18 +1,16 @@
 'use strict';
-​
-​
 const path = require('path');
 const mainFile = path.basename(require.main.filename);
-​
+
 //change log date every month
 //clear file using echo "" > log-file-name
 //in the format YYYY-MM
 const now = new Date();
 const logStartDate = `${now.getFullYear()}-${(now.getMonth() + 1)}`;
 const loggerName = `logs-${logStartDate}`;
-​
+
 let streams = null;
-​
+
 if (process.env.NODE_ENV === 'production') {
   streams = [{path: 'logs/' + loggerName + '.log'}];
 } else if(process.env.NODE_ENV === 'production' && process.env.MACHINE === 'heroku'){
@@ -25,7 +23,7 @@ if (process.env.NODE_ENV === 'production') {
     {stream: process.stdout}
   ];
 }
-​
+
 module.exports = require('bunyan').createLogger({
   name: loggerName,
   streams: streams
