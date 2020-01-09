91ecc32798819acfdf8d64ae47700cac784d3be8
Nipan Das <nipandas83@gmail.com>
2020-01-09 08:15:49 +0530

    enabled put /user/location API

--- app/routes/user.js
+++ app/routes/user.js
@@ -27,7 +27,7 @@ router.post('/complete-registration', multerStart(fieldsFile), controller.comple
 router.put('/edit', authService.authorize, multerStart(fieldsFile), controller.put);
 
 router.get('/', authService.authorize, controller.getAuthUserMiddleware, controller.getUser);
-//router.put('/location', authService.authorize, controller.getAuthUserMiddleware, controller.updateLocation);
+router.put('/location', authService.authorize, controller.getAuthUserMiddleware, controller.updateLocation);
 //auth routes
 router.post('/login', authController.authenticate);
 router.post('/logout', authService.authorize, authController.logout);
