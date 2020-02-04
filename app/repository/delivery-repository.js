9eec864c9c3c1d7da7d56ee083242756c8d2053c
Nipan Das <nipandas83@gmail.com>
2020-02-05 00:30:23 +0530

    Revert "updated order delivery api"
    This reverts commit 0d55e9985672c2a49230bb507a887b27c9321f2d.

--- app/repository/delivery-repository.js
+++ app/repository/delivery-repository.js
@@ -63,7 +63,7 @@ exports.getOrderDeliveriesByUserId = async (data,driver) => {
     },
     {
     model: OrderDelivery,
-    required: false,
+    required: true,
     attributes: ["id","state_type"]
     //where: {state_type: 'pending'}
    }]
