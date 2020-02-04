b28b0bae1681a1d848a8a34d3a24800e6ede9ed6
Nipan Das <nipandas83@gmail.com>
2020-02-05 00:30:07 +0530

    Revert "Revert "modified plate/favourite APIs""
    This reverts commit 3021b69db2b322f91d181f45e6d7d2868bcda7b6.

--- app/controlers/favourite-controler.js
+++ app/controlers/favourite-controler.js
@@ -52,7 +52,7 @@ exports.favourite = async (req, res, next) => {
       return 0;
     }
 
-    let existPlateInFav = await repoFav.findPlateinFav(req.body.plateId);
+    let existPlateInFav = await repoFav.findPlateinFav(req.body.plateId, req.userId);
     if (existPlateInFav) {
       res.status(HttpStatus.CONFLICT).send({ message: "Already exists in favourites", status: HttpStatus.CONFLICT});
       return 0;
@@ -76,7 +76,7 @@ exports.favourite = async (req, res, next) => {
       return 0;
     }
 
-    let existCustomPlateInFav = await repoFav.findCustomPlateinFav(req.body.CustomplateId);
+    let existCustomPlateInFav = await repoFav.findCustomPlateinFav(req.body.CustomplateId, req.userId);
     if (existCustomPlateInFav) {
       res.status(HttpStatus.CONFLICT).send({ message: "Already exists in favourites", status: HttpStatus.CONFLICT});
       return 0;
@@ -136,13 +136,13 @@ exports.removeFavourite = async (req, res, next) => {
       return 0;
     }
 
-    let existPlateInFav = await repoFav.findPlateinFav(req.params.id);
+    let existPlateInFav = await repoFav.findPlateinFav(req.params.id, req.userId);
     if (!existPlateInFav) {
       res.status(HttpStatus.CONFLICT).send({ message: "Cann't find in favourites", status: HttpStatus.CONFLICT});
       return 0;
     }
 
-    response = await repoFav.delete(existPlateInFav.id);
+    response = await repoFav.delete(existPlateInFav.id, req.userId);
 
 
     }
@@ -160,14 +160,14 @@ exports.removeFavourite = async (req, res, next) => {
       return 0;
     }
 
-    let existCustomPlateInFav = await repoFav.findCustomPlateinFav(req.params.id);
+    let existCustomPlateInFav = await repoFav.findCustomPlateinFav(req.params.id, req.userId);
     if (!existCustomPlateInFav) {
       res.status(HttpStatus.CONFLICT).send({ message: "Can't find in favourites", status: HttpStatus.CONFLICT});
       return 0;
     }
 
 
-    response = await repoFav.delete(existCustomPlateInFav.id);
+    response = await repoFav.delete(existCustomPlateInFav.id,req.userId);
 
 
     }
