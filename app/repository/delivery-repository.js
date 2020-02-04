5057f06972e3faba69b388b0f60400ac5a2bf5f9
Nipan Das <nipandas83@gmail.com>
2020-02-05 00:00:23 +0530

    updated order delivery api

--- app/repository/delivery-repository.js
+++ app/repository/delivery-repository.js
@@ -63,7 +63,7 @@ exports.getOrderDeliveriesByUserId = async (data,driver) => {
     },
     {
     model: OrderDelivery,
-    required: true,
+    required: false,
     attributes: ["id","state_type"]
     //where: {state_type: 'pending'}
    }]
