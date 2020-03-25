b68a70a43a6d6c0d07e42e49756b02b9f57dbe83
Sihyun <sihyunchae0@gmail.com>
2020-03-25 14:27:31 -0400

    updates

---
+++ app/models/promotions.js
@@ -0,0 +1,35 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+	const Promotions = sequelize.define(
+		"Promotions",
+		{
+			name: {
+				allowNull: false,
+				type: DataTypes.STRING
+			},
+			code: {
+				allowNull: false,
+				type: DataTypes.STRING,
+				unique: true
+			},
+			discount: {
+				allowNull: false,
+				type: DataTypes.DOUBLE
+			},
+			status: {
+				allowNull: false,
+				defaultValue: false,
+				type: DataTypes.BOOLEAN
+			},
+			validity: {
+				allowNull: false,
+				type: DataTypes.DATE
+			}
+		},
+		{}
+	);
+	Promotions.associate = function(models) {
+		// associations can be defined here
+	};
+	return Promotions;
+};
