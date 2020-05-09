e74761b6001a9f3de02c9f2cbfd078126acbcbda
Krushn <kathrechakrushn@gmail.com>
2020-05-10 02:50:06 +0530

    db settings

--- app/models/index.js
+++ app/models/index.js
@@ -5,13 +5,13 @@ const config = require(path.resolve('config/database.js'));
 
 const db = {};
 
-//const sequelize = new Sequelize(config.mysql);
+const sequelize = new Sequelize(config.mysql);
 
-    const sequelize = new Sequelize('c821iyxdz9lgx1ut', 'n2ad1m9h9pnmrnhx', 'tfmdz7w8adok6587', {
-      host: "umabrisfx8afs3ja.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",//config.mysql.host,
-    //  port: '8889',//config.mysql.port,
-      dialect: 'mysql'//config.mysql.dialect
-    });
+/*  const sequelize = new Sequelize('c821iyxdz9lgx1ut', 'n2ad1m9h9pnmrnhx', 'tfmdz7w8adok6587', {
+    host: "umabrisfx8afs3ja.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",//config.mysql.host,
+  //  port: '8889',//config.mysql.port,
+    dialect: 'mysql'//config.mysql.dialect
+  });*/
     
 fs
   .readdirSync(__dirname)
