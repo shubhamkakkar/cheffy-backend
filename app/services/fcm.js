825a78a6219e8902e4540aa481a5d1d2bc695931
shubhamkakkar <shubham.kakkar98@gmail.com>
2020-05-28 22:37:57 +0530

    changes added in fcm for android and ios

--- app/services/fcm.js
+++ app/services/fcm.js
@@ -1,37 +1,40 @@
-const path = require('path');
-const fcmAPI = require(path.resolve('config/fcmSettings.json')).fcm;
-const FCM = require('fcm-push');
-const { Notification, sequelize } = require(path.resolve('app/models/index'));
+const path = require("path");
+const fcmAPI = require(path.resolve("config/fcmSettings.json")).fcm;
+const FCM = require("fcm-push");
+const { Notification, sequelize } = require(path.resolve("app/models/index"));
 
 /*For Notifications*/
 
 module.exports = async (data) => {
-	const fcm = new FCM(fcmAPI.serverKey);
-	const message = {
-		registration_ids: data.device_registration_tokens, // Multiple tokens in an array
-		notification: {
-			title: data.orderTitle,
-			body: data.orderBrief,
-		},
-	};
-	try {
-		const res = await fcm.send(message);
-	} catch (err) {
-		console.log('Something has gone wrong!', err);
-	} finally {
-		saveNotification(data);
-	}
+  const fcm = new FCM(fcmAPI.serverKey);
+  const message = {
+    registration_ids: data.device_registration_tokens, // Multiple tokens in an array
+    notification: {
+      title: data.orderTitle,
+      body: data.orderBrief,
+      // "android_channel_id": "500", android > 8.0 requirement
+    },
+    priority: "high", // android < 8.0 requirement
+    content_available: true, // ios requirement
+  };
+  try {
+    const res = await fcm.send(message);
+  } catch (err) {
+    console.log("Something has gone wrong!", err);
+  } finally {
+    saveNotification(data);
+  }
 };
 
 const saveNotification = (data) => {
-	const notifications = data.detail.map((element) => {
-		return {
-			timestamp: sequelize.literal('CURRENT_TIMESTAMP'),
-			orderTitle: data.orderTitle,
-			orderBrief: data.orderBrief,
-			userId: element.userId,
-			device_id: element.deviceId,
-		};
-	});
-	Notification.bulkCreate(notifications);
+  const notifications = data.detail.map((element) => {
+    return {
+      timestamp: sequelize.literal("CURRENT_TIMESTAMP"),
+      orderTitle: data.orderTitle,
+      orderBrief: data.orderBrief,
+      userId: element.userId,
+      device_id: element.deviceId,
+    };
+  });
+  Notification.bulkCreate(notifications);
 };
