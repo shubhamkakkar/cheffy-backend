3e31c18981c2775e491832f35cb786dfd2862348
shubhamkakkar <shubham.kakkar98@gmail.com>
2020-05-30 12:03:04 +0530

    delivery -> /time && /price/calculate made autherizeOptional

--- app/routes/payment.js
+++ app/routes/payment.js
@@ -1,13 +1,18 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const authService = require("../services/auth");
-const controller = require('../controlers/payment-controler');
+const controller = require("../controlers/payment-controler");
 
-router.get('/paypal/callback', controller.callback);
-router.get('/paypal/:orderId', authService.authorize, controller.startPaymentPaypal);
-router.post('/paypal/confirm', controller.confirm);
-router.get('/paypal/cancel', controller.cancel);
+//TODO: need information on paypal payments
+router.get("/paypal/callback", controller.callback);
+router.get(
+  "/paypal/:orderId",
+  authService.authorize,
+  controller.startPaymentPaypal
+);
+router.post("/paypal/confirm", controller.confirm);
+router.get("/paypal/cancel", controller.cancel);
 
 module.exports = router;
