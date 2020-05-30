62b67a473b56dd90da3b05d59422c93292821410
shubhamkakkar <shubham.kakkar98@gmail.com>
2020-05-30 13:19:14 +0530

    rating checked - all authenticated

--- app/routes/rating.js
+++ app/routes/rating.js
@@ -1,15 +1,14 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 
 const authService = require("../services/auth");
-const controller = require('../controlers/rating-controler');
-
+const controller = require("../controlers/rating-controler");
 
 const router = express.Router();
 
-router.get('/:review_type/:id', authService.authorize, controller.getRating);
+router.get("/:review_type/:id", authService.authorize, controller.getRating);
 
-router.post('/', authService.authorize, controller.postRating);
-    
+router.post("/", authService.authorize, controller.postRating);
+
 module.exports = router;
