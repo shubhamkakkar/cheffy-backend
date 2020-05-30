37782e3bf8319a287c54936874a569c25c6a8a5d
shubhamkakkar <shubham.kakkar98@gmail.com>
2020-05-30 12:26:18 +0530

    favourite.js checked for non token routes - found none

--- app/routes/favourite.js
+++ app/routes/favourite.js
@@ -1,15 +1,18 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 
 const authService = require("../services/auth");
-const controller = require('../controlers/favourite-controler');
-
+const controller = require("../controlers/favourite-controler");
 
 const router = express.Router();
 
-router.post('/add', authService.authorize, controller.favourite);
-router.delete('/remove/:fav_type/:id', authService.authorize, controller.removeFavourite);
-router.get('/', authService.authorize, controller.list);
-    
+router.post("/add", authService.authorize, controller.favourite);
+router.delete(
+  "/remove/:fav_type/:id",
+  authService.authorize,
+  controller.removeFavourite
+);
+router.get("/", authService.authorize, controller.list);
+
 module.exports = router;
