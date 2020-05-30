fabd86e678b101891ca7800e775f27ca06bfdc43
shubhamkakkar <shubham.kakkar98@gmail.com>
2020-05-30 11:27:07 +0530

    added authorizeOptional to routes - plate.js

--- app/routes/plate.js
+++ app/routes/plate.js
@@ -1,110 +1,131 @@
-'use strict';
+"use strict";
 
-const path = require('path');
-const express = require('express');
-const controller = require('../controlers/plate-controler');
-const authService = require('../services/auth');
-const multerStart = require('../../config/multer_cloudinary');
+const path = require("path");
+const express = require("express");
+const controller = require("../controlers/plate-controler");
+const authService = require("../services/auth");
+const multerStart = require("../../config/multer_cloudinary");
 const shippingController = require(path.resolve(
-	'app/controlers/shipping-controler'
+  "app/controlers/shipping-controler"
 ));
-const userController = require(path.resolve('app/controlers/user-controler'));
+const userController = require(path.resolve("app/controlers/user-controler"));
 const categoryController = require(path.resolve(
-	'app/controlers/category-controler'
+  "app/controlers/category-controler"
 ));
-const platePolicies = require(path.resolve('app/policies/plate'));
+const platePolicies = require(path.resolve("app/policies/plate"));
 
 const imageFields = [
-	'profile_photo',
-	'kitchen_image',
-	'receipt_image',
-	'plate_image',
+  "profile_photo",
+  "kitchen_image",
+  "receipt_image",
+  "plate_image",
 ];
 
 const fieldsFile = imageFields.map((field) => {
-	return { name: field, maxCount: 10 };
+  return { name: field, maxCount: 10 };
 });
 
 const router = express.Router();
 
 router.post(
-	'/',
-	authService.authorize,
-	shippingController.getAuthUserShippingAddress,
-	controller.create
+  "/",
+  authService.authorize,
+  shippingController.getAuthUserShippingAddress,
+  controller.create
 );
 
 router.get(
-	'/',
-	authService.authorizeOptional,
-	userController.getAuthUserIfPresentMiddleware,
-	controller.list
+  "/",
+  authService.authorizeOptional,
+  userController.getAuthUserIfPresentMiddleware,
+  controller.list
 );
 
 router.get(
-	'/help',
-	authService.authorizeOptional,
-	userController.getAuthUserIfPresentMiddleware,
-	controller.searchHelp
+  "/help",
+  authService.authorizeOptional,
+  userController.getAuthUserIfPresentMiddleware,
+  controller.searchHelp
 );
 
 router.get(
-	'/show/:id',
-	authService.authorizeOptional,
-	userController.getAuthUserIfPresentMiddleware,
-	controller.getPlate
+  "/show/:id",
+  authService.authorizeOptional,
+  userController.getAuthUserIfPresentMiddleware,
+  controller.getPlate
 );
 
 router.post(
-	'/edit/:id',
-	authService.authorize,
-	userController.getAuthUserMiddleware,
-	platePolicies.isOwnerMiddleware(),
-	controller.edit
+  "/edit/:id",
+  authService.authorize,
+  userController.getAuthUserMiddleware,
+  platePolicies.isOwnerMiddleware(),
+  controller.edit
 );
 
 router.delete(
-	'/:id',
-	authService.authorize,
-	userController.getAuthUserMiddleware,
-	platePolicies.isOwnerMiddleware(),
-	controller.delete
+  "/:id",
+  authService.authorize,
+  userController.getAuthUserMiddleware,
+  platePolicies.isOwnerMiddleware(),
+  controller.delete
 );
 
 router.post(
-	'/images/:id',
-	authService.authorize,
-	userController.getAuthUserMiddleware,
-	multerStart(fieldsFile),
-	platePolicies.isOwnerMiddleware(),
-	controller.uploadImages
+  "/images/:id",
+  authService.authorize,
+  userController.getAuthUserMiddleware,
+  multerStart(fieldsFile),
+  platePolicies.isOwnerMiddleware(),
+  controller.uploadImages
 );
 
 //TODO add permission
 router.delete(
-	'/:id/images/:type_image/:plateImageId',
-	authService.authorize,
-	userController.getAuthUserMiddleware,
-	platePolicies.isOwnerMiddleware(),
-	controller.deleteImage
+  "/:id/images/:type_image/:plateImageId",
+  authService.authorize,
+  userController.getAuthUserMiddleware,
+  platePolicies.isOwnerMiddleware(),
+  controller.deleteImage
 );
 
-router.get('/:id/kitchen', controller.imagePlateKitchen);
-router.get('/:id/review', controller.getPlateReview);
-router.get('/:id/related', controller.getRelatedPlates);
-router.get('/:id/receipt', controller.listReceipt);
-
-router.get('/category/:categoryId', controller.categoryPlates);
 router.get(
-	'/chef/:chefId',
-	authService.authorizeOptional,
-	userController.getAuthUserIfPresentMiddleware,
-	controller.getChefPlates
+  "/:id/kitchen",
+  authService.authorizeOptional,
+  controller.imagePlateKitchen
 );
-router.get('/popular', controller.popularPlates);
+router.get(
+  "/:id/review",
+  authService.authorizeOptional,
+  controller.getPlateReview
+);
+router.get(
+  "/:id/related",
+  authService.authorizeOptional,
+  controller.getRelatedPlates
+);
+router.get(
+  "/:id/receipt",
+  authService.authorizeOptional,
+  controller.listReceipt
+);
 
-router.param('categoryId', categoryController.categoryByIdMiddleware);
-router.param('chefId', userController.getUserByUserIdParamMiddleware);
-router.param('id', controller.getPlateByIdMiddleware);
+router.get(
+  "/category/:categoryId",
+  authService.authorizeOptional,
+  controller.categoryPlates
+);
+router.get(
+  "/chef/:chefId",
+  authService.authorizeOptional,
+  userController.getAuthUserIfPresentMiddleware,
+  controller.getChefPlates
+);
+router.get("/popular", authService.authorizeOptional, controller.popularPlates);
+
+//TODO: need more info
+router.param("categoryId", categoryController.categoryByIdMiddleware);
+router.param("chefId", userController.getUserByUserIdParamMiddleware);
+router.param("id", controller.getPlateByIdMiddleware);
 
 module.exports = router;
