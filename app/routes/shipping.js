c6441ed19db2221b1bb5dadc0af35372a864029f
shubhamkakkar <shubham.kakkar98@gmail.com>
2020-05-30 11:47:27 +0530

    shipping.js checked for non token routes - found none

--- app/routes/shipping.js
+++ app/routes/shipping.js
@@ -1,20 +1,48 @@
 "use strict";
 
-const path = require('path');
+const path = require("path");
 const express = require("express");
 const router = express.Router();
 const controller = require("../controlers/shipping-controler");
 const authService = require("../services/auth");
-const userController = require(path.resolve('app/controlers/user-controler'));
-const shippingPolicies = require(path.resolve('app/policies/shipping-address'));
+const userController = require(path.resolve("app/controlers/user-controler"));
+const shippingPolicies = require(path.resolve("app/policies/shipping-address"));
 
-router.post("/", authService.authorize, userController.getAuthUserMiddleware, controller.create);
-router.get("/", authService.authorize, userController.getAuthUserMiddleware, controller.list);
+router.post(
+  "/",
+  authService.authorize,
+  userController.getAuthUserMiddleware,
+  controller.create
+);
+router.get(
+  "/",
+  authService.authorize,
+  userController.getAuthUserMiddleware,
+  controller.list
+);
 
-router.put("/:shippingAddressId", authService.authorize, userController.getAuthUserMiddleware, shippingPolicies.isOwnerMiddleware(), controller.edit);
-router.put("/:shippingAddressId/set-default", authService.authorize, userController.getAuthUserMiddleware, shippingPolicies.isOwnerMiddleware(), controller.setDefaultAddress);
-router.delete("/:shippingAddressId", authService.authorize, userController.getAuthUserMiddleware, shippingPolicies.isOwnerMiddleware(), controller.remove);
+router.put(
+  "/:shippingAddressId",
+  authService.authorize,
+  userController.getAuthUserMiddleware,
+  shippingPolicies.isOwnerMiddleware(),
+  controller.edit
+);
+router.put(
+  "/:shippingAddressId/set-default",
+  authService.authorize,
+  userController.getAuthUserMiddleware,
+  shippingPolicies.isOwnerMiddleware(),
+  controller.setDefaultAddress
+);
+router.delete(
+  "/:shippingAddressId",
+  authService.authorize,
+  userController.getAuthUserMiddleware,
+  shippingPolicies.isOwnerMiddleware(),
+  controller.remove
+);
 
-router.param('shippingAddressId', controller.getShippingAddressByIdMiddleware);
+router.param("shippingAddressId", controller.getShippingAddressByIdMiddleware);
 
 module.exports = router;
